8e5f33653a270448b1f0f79c84c06ed686f97b12
Riaan Adriaanse <riaan@al.co.za>
2026-08-19 09:47:36 +0000

    added station dimension

---
+++ models/example/station_dimension.sql
@@ -0,0 +1,14 @@
+with BIKE as (
+
+    select
+    distinct
+    START_STATIO_ID as station_id,
+    start_station_name as station_name,
+    START_LAT as station_lat,
+    START_LNG as start_station_lng
+    from {{ source('demo', 'bike') }}
+    where RIDE_ID != 'ride_id'
+    limit 10
+)
+
+SELECT * from BIKE
